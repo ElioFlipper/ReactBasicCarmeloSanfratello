d2f1ae2c6bd4aaebad20ff26bbd7dc9668839c1d
Elio <elio.sanfratello@gmail.com>
2023-12-06 17:06:26 +0100

    components2

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -1,11 +1,19 @@
 export function App() {
     return (
-    <div>
-        <Hello />
+        <div>
+            <Hello />
         </div>
-        )
+    )
 }
 
-function Hello() {
-    return <h2>Hello, world!</h2>;
+function Message() {
+    return <p>What a beautiful day!</p>
+}
+
+export function Hello() {
+    return (
+        <div>
+            <h2>Hello, world!</h2>
+            <Message />
+        </div>)
 }
