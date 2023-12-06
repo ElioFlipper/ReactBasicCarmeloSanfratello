d9330b2675a1e92bcbdc8237fd11d434eaf42f96
Elio <elio.sanfratello@gmail.com>
2023-12-06 17:20:58 +0100

    Components4

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -2,12 +2,9 @@ export function App() {
     return (
         <div>
             <Hello />
-            <Message />
         </div>
     )
 }
-//Se richiamo due volte la funzione <Hello />, stampo due volte l'H2 "Hello, world!"
-//Allo stesso modo, se richiamo <Message /> in entrambe le funzioni, verrà renderizzato molteplici volte
 
 function Message() {
     return <p>What a beautiful day!</p>
