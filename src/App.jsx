5ee1db6ed6e7abba97cd6894b8778ee3af7df66e
Elio <elio.sanfratello@gmail.com>
2023-12-07 16:39:28 +0100

    Props1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import { Welcome } from "./Welcome";
+
+export function App() {
+    return(
+        <div>
+            <Welcome name = "Elio" />
+        </div>
+    )
+}
+//Se non passo alcun name al component "Welcome", non verrà visualizzato nulla
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import { App } from "./HelloWorld"
 import {createRoot} from "react-dom/client"
-
 createRoot(document.getElementById('root')).render(<App />)
+
+import { App } from "./App";
