061830079a13f5b3d7163166249f6f57cac33b2a
Elio <elio.sanfratello@gmail.com>
2023-12-08 15:42:35 +0100

    Conditional rendering 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Welcome } from "./Welcome";
 export function App() {
     return(
         <div>
-            <Welcome name = "John" eta = {55}/>
+            <Welcome name = "John" eta = {15}/>
         </div>
     )
 }
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -4,11 +4,7 @@ export function Welcome({ name, eta }) {
     return (
         <div>
             <p>Welcome, <strong>{name}!</strong></p>
-            <Age eta={eta} />
-            {eta > 18 && <Age eta={eta} />}
-            {eta && <Age eta={eta} />}
-            {eta>18 && eta<65  && <Age eta = {eta} />}
-            {eta>18 && eta<65 && name === "John"  && <Age eta = {eta} />}
+            {eta>18 ? <Age eta={eta}/> : <p>You are very young!</p>}
 
         </div>
     )
