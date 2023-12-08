b1df13b8565d0fc3c5e22fbec6b2b65d046c279d
Elio <elio.sanfratello@gmail.com>
2023-12-08 18:15:52 +0100

    Ho sbagliato

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
+// import { AlertClock } from "./AlertClock";
 import { Welcome } from "./Welcome";
 
 export function App() {
     return(
         <div>
             <Welcome name = "John" eta = {15}/>
+            {/* <AlertClock /> */}
         </div>
     )
 }
