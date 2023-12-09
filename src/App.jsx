694ad99e164476bd38949c38356b29d73c1868b5
Elio <elio.sanfratello@gmail.com>
2023-12-09 17:26:43 +0100

    HandlingEvents1

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@
 import { AlertClock } from "./AlertClock";
 import { Clock } from "./Clock";
 import { Counter } from "./Counter";
+import { MouseClicker } from "./MouseClicker";
 import { Welcome } from "./Welcome";
 
 export function App() {
     return (
         <div>
-            <Clock />
+            <MouseClicker />
         </div>
     )
 }
