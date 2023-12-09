c2a7fbe6ff355916694b1f24dac188bf61de52da
Elio <elio.sanfratello@gmail.com>
2023-12-09 17:40:52 +0100

    Handling Events2

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,11 +1,13 @@
 export function MouseClicker(){
     function handleButtonClick(event){
-        console.log(event.currentTarget.name)
+        console.log(event.currentTarget.src)
     }
 
     return (
         <div>
-            <button name = "Elio" onClick={handleButtonClick}>Cliccami</button>
+            <button name = "Elio" onClick={handleButtonClick}>
+                <img src = "https://www.fisiostore.it/images/pesi-manubri-web_3987.jpg" width={24} height={24} onClick={handleButtonClick}/>
+                Cliccami</button>
         </div>
     )
 }
