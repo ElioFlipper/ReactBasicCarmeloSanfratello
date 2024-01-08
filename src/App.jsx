707296e44cf141dd3b29e95561ff71367b6711bf
Elio <elio.sanfratello@gmail.com>
2024-01-08 10:47:04 +0100

    Router1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
+import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 
 export function App() {
     return(
-        <div>
-            <Welcome name = "Elio" />
-        </div>
+<Routes> 
+    <Route path="/" element={<Welcome name="Eliuccio"/>} />
+</Routes>
     )
 }
 //Se non passo alcun name al component "Welcome", non verrà visualizzato nulla
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
-import {createRoot} from "react-dom/client"
-createRoot(document.getElementById('root')).render(<App />)
+import { createRoot } from "react-dom/client"
+createRoot(document.getElementById('root')).render(<Root />)
 
-import { App } from "./App";
+import { Root } from "./BrowserRouter";
+
